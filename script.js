5efcfbe83324ce6c69544c3a236a024bc4efed38
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-10-20 01:59:29 -0300

    implementação do menu e do modo escuro
    implementei o menu, ficou faltando dar estilo, e implementei o modo escuro, falta finalizar algumas alterações.

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+let menuButton = document.getElementById("menuButton");
+let menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", function(){
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+});
+
+let areaServicos = document.getElementById("areaServicos");
+let menuServicos = document.getElementById("menuServicos");
+
+areaServicos.addEventListener("click", function(){
+    if(menuServicos.style.display === "block"){
+        menuServicos.style.display = "none";
+    }else{
+        menuServicos.style.display = "block";
+    }
+});
+*/
+
+let trilho = document.getElementById('trilho')
+let body =document.querySelector('body')
+trilho.addEventListener('click', ()=> {
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
