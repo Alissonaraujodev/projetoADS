c71f0b0e11ab362fd45c69d52918d644ad626cc4
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-10-20 23:09:39 -0300

    modo escuro
    continuação do modo escuro, organizei as cores de fundo, dos textos e dos títulos do modo claro e do modo escuro, comecei a mexer na área do menu

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ menuButton.addEventListener("click", function(){
         menu.style.display = "block";
     }
 });
-
+*/
 let areaServicos = document.getElementById("areaServicos");
 let menuServicos = document.getElementById("menuServicos");
 
@@ -20,7 +20,6 @@ areaServicos.addEventListener("click", function(){
         menuServicos.style.display = "block";
     }
 });
-*/
 
 let trilho = document.getElementById('trilho')
 let body =document.querySelector('body')
@@ -28,3 +27,9 @@ trilho.addEventListener('click', ()=> {
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
 })
+
+function voltarPaginaAnterior(){
+    location.replace(document.referrer)
+}
+document.getElementById('botao-Voltar')
+addEventListener("click",voltarPaginaAnterior)
