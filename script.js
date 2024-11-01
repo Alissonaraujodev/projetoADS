87d2d1ac4432ceef14590db2bb762bf2efb82b2c
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-11-01 17:27:33 -0300

    carrossel de imagens, organização de estilo e páginas
    Alterei o carrossel e coloquei em todas as páginas que tem imagens, fiz as páginas de eventos culturais, esportivos e educação e coloquei as cores na pasta do modo escuro

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-/*
-let menuButton = document.getElementById("menuButton");
-let menu = document.getElementById("menu");
 
-menuButton.addEventListener("click", function(){
-    if(menu.style.display === "block"){
-        menu.style.display = "none";
-    }else{
-        menu.style.display = "block";
-    }
-});
-*/
-/*
-let areaServicos = document.getElementById("areaServicos");
-let menuServicos = document.getElementById("menuServicos");
-
-areaServicos.addEventListener("click", function(){
-    if(menuServicos.style.display === "block"){
-        menuServicos.style.display = "none";
-    }else{
-        menuServicos.style.display = "block";
-    }
-});
-*/
-/*
+//função para modo escuro
 let trilho = document.getElementById('trilho');
 let body = document.querySelector('body');
 if(localStorage.getItem('darkMode')=== 'enabled'){
@@ -40,11 +17,24 @@ trilho.addEventListener('click', ()=> {
         localStorage.setItem('darkMode', 'disable');
     }
 })
-*/
-/*
-function voltarPaginaAnterior(){
-    location.replace(document.referrer)
+
+//função para carrossel de imagens
+const track = document.querySelector('.carousel-track');
+const prevButton = document.querySelector('.carousel-button.prev');
+const nextButton = document.querySelector('.carousel-button.next');
+const images = Array.from(track.children);
+let currentIndex = 0;
+
+function updateCarousel() {
+    const slideWidth = images[0].getBoundingClientRect().width;
+    track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
 }
-document.getElementById('botao-Voltar')
-addEventListener("click",voltarPaginaAnterior)
-*/
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarousel();
+});
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateCarousel();
+});
+
